67f63a782fe3f288b1a23788eae3df10fa52c5c8
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-08-09 00:59:33 +0600

    create WhyChooseUsSection

---
+++ src/app/components/WhyChooseUs.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import musicSchoolContent from '@/app/data/music_school_content.json'
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+
+
+const content = musicSchoolContent.data.map((item) => ({
+    title: item.title,
+    description: item.description,
+    content: item.content ? (
+        <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
+            <p className="text-center text-lg p-4">{item.content}</p>
+        </div>
+    ) : (
+        <div className="flex h-full w-full items-center justify-around text-white">
+            <p className="text-center text-md">{item.title}</p>
+            <img
+                src={item.img}
+                width={250}
+                height={250}
+                className="h-full w-1/2 object-cover"
+                alt={item.title}
+            />
+           
+        </div>
+    )
+}));
+
+
+function WhyChooseUs(){
+  return (
+    <div  className="w-full">
+        <StickyScroll content={content} />
+    </div>
+  )
+}
+
+export default WhyChooseUs
