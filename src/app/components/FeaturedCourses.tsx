0099203ab8a3e6dc6684f0d754ffcaf5221127d7
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-08-17 16:43:40 +0600

    add Link to the Featured course image in:FeaturedCourses.tsx

--- src/app/components/FeaturedCourses.tsx
+++ src/app/components/FeaturedCourses.tsx
@@ -35,14 +35,16 @@ const FeaturedCourses = () => {
                         featuredCourses.map((course: Course) => (
                             <div key={course.id} className="flex justify-center">
                                 <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm" >
-                                    <img
-                                        src={course.image}
-                                        alt={course.title}
-                                        height="400"
-                                        width="400"
-                                        className="object-cover"
-                                    />
-                                    
+                                    <Link href={`/courses/${course.slug}`}>
+                                        <img
+                                            src={course.image}
+                                            alt={course.title}
+                                            height="400"
+                                            width="400"
+                                            className="object-cover"
+                                        />   
+                                    </Link>
+
                                     <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                                         <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                                         <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
